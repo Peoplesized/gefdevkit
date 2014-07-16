ecef4539993edc9b8fda06fddfae747c5301675f
Jonathan Smith <jonathan@peoplesized.com>
2014-07-16 22:16:47 +0200

    compiled

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,1 @@
-(function() {
-  console.log("hello from a require'd coffee file (via assets/js/_helper.coffee)");
-
-}).call(this);
-
-(function() {
-  console.log("Done");
-
-}).call(this);
+(function(){console.log("hello from a require'd coffee file (via assets/js/_helper.coffee)")}).call(this),function(){console.log("Done")}.call(this)
